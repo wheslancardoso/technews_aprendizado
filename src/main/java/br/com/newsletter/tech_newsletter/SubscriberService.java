4d17513674859ca136e7c2a0fe8edf619de02336
wheslancardoso <wheslancardoso1@gmail.com>
2025-09-04 19:42:37 -0300

    criacao subscribeService

---
+++ src/main/java/br/com/newsletter/tech_newsletter/SubscriberService.java
@@ -0,0 +1,10 @@
+package br.com.newsletter.tech_newsletter;
+
+import org.springframework.stereotype.Service;
+
+@Service // Diz ao Spring que esta é a nossa classe de serviço
+public class SubscriberService {
+
+    // A lógica virá para aqui
+
+}
